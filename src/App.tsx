39f2764f6d0d50d646364d0e4ed85531b211ddf9
Ian Chu <chu2815@gmail.com>
2022-03-08 23:28:11 +0800

    Edit update full mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,25 @@ import { useEffect, useState } from "react";
 import SplitPane, { Pane } from "react-split-pane";
 import data from "./data";
 import "./App.less";
+import { CloseOutlined } from "@ant-design/icons";
 
 import Dashboard from "./Dashboard";
 import Editor from "./Editor";
 
-function buildLinklist(mock, children, rowData) {
+const buildLinklist = (mock, children, rowData) => {
   if (children && Array.isArray(children)) {
     mock.children = [];
     return {
       ...mock,
       children: children.map((id) => {
-        const selectItem = rowData.find((item) => item.id === id);
+        const selectItem = rowData.find((item: { id: any }) => item.id === id);
         const { children: childrens, ...rest } = selectItem;
         return buildLinklist({ ...rest }, childrens, rowData);
       }),
     };
   }
   return { ...mock };
-}
+};
 
 const App = () => {
   const [selectEdit, setSelectEdit] = useState("");
@@ -27,44 +28,64 @@ const App = () => {
   let { children, ...restProps } =
     (code && code.find((item) => item.root === true)) || {};
   const buildTree = buildLinklist(restProps, children, code);
-  console.log(`buildTree`, buildTree);
 
-  const handleSetEditUpdate = (value) => {
-    setSelectEdit(value);
-    console.log(value);
-    const mockCode = [...code];
+  const findItemUpdate = (
+    mockCode: any[],
+    value: { [x: string]: any; id?: any; children?: any }
+  ) => {
     const { children, ...restValue } = value;
     mockCode[mockCode.findIndex((el) => el.id === value.id)] = {
       ...mockCode[mockCode.findIndex((el) => el.id === value.id)],
       ...restValue,
     };
+  };
+
+  const recursiveUpdateDown = (
+    mockCode: any[],
+    value: { children: { [x: string]: any; id?: any; children?: any }[] }
+  ) => {
     if (value?.children) {
-      value.children.forEach((element) => {
-        const { children, ...restValue } = element;
-        console.log(restValue);
-        mockCode[mockCode.findIndex((el) => el.id === element.id)] = {
-          ...mockCode[mockCode.findIndex((el) => el.id === element.id)],
-          ...restValue,
-        };
-      });
+      value.children.forEach(
+        (element: { [x: string]: any; id?: any; children?: any }) => {
+          findItemUpdate(mockCode, element);
+          recursiveUpdateDown(mockCode, element);
+        }
+      );
     }
-    console.log(mockCode);
-    setCode(mockCode);
+  };
+
+  const handleSetEditUpdate = (value, callback = () => {}) => {
+    setSelectEdit(value);
+    const mockCode = [...code];
+    findItemUpdate(mockCode, value);
+    recursiveUpdateDown(mockCode, value);
+    callback(mockCode);
   };
 
   return (
     <SplitPane split="vertical" minSize={`50%`} defaultSize={`50%`}>
       <Pane initialSize="40%" minSize="10%">
-        You can use a Pane component
-        <h3>Data edit</h3>
+        <div>
+          <h3>Data edit</h3>
+          {selectEdit?.id && (
+            <>
+              You are in selected edit mode
+              <CloseOutlined onClick={() => setSelectEdit("")} />
+            </>
+          )}
+        </div>
         {selectEdit ? (
           <Editor
             key={selectEdit?.id}
             code={selectEdit}
-            updateCode={handleSetEditUpdate}
+            updateCode={(val) => handleSetEditUpdate(val, setCode)}
           />
         ) : (
-          <Editor key={buildTree?.id} code={buildTree} updateCode={setCode} />
+          <Editor
+            key={buildTree?.id}
+            code={buildTree}
+            updateCode={(val) => handleSetEditUpdate(val, setCode)}
+          />
         )}
       </Pane>
       <Pane initialSize="60%" minSize="10%">
